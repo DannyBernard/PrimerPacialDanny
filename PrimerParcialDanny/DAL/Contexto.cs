968795c2f923dcb23aefeb0370726976f1956cbe
DANNY BERNARD <DANNY.BERNARD@hotmail.com>
2018-09-16 19:15:59 +0200

    Primer Parcial

---
+++ PrimerParcialDanny/DAL/Contexto.cs
@@ -0,0 +1,21 @@
+﻿using PrimerParcialDanny.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace PrimerParcialDanny.DAL
+{
+    class Contexto: DbContext
+    {
+        public DbSet<Articulos> Articulos { get; set; }
+
+        public Contexto() : base("ConStr")
+       {
+       }
+
+    }
+}
---
+++ PrimerParcialDanny/Entidades/Articulos.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimerParcialDanny.Entidades
+{
+    class Articulos
+    {
+        public int ID { get; set; }
+        public string Descipcion { get; set; }
+        public decimal Precio { get; set; }
+        public int Cantidad { get; set; }
+        public DateTime FechaDeVencimiento { get; set; }
+
+        public Articulos()
+        {
+            ID = 0;
+            Descipcion = string.Empty;
+            Precio = 0;
+            Cantidad = 0;
+            FechaDeVencimiento = DateTime.Now;
+
+        }
+    }
+}

86826820c8bf7ac080185272a2300a2d4edf1937
DANNY BERNARD <DANNY.BERNARD@hotmail.com>
2018-09-18 00:26:31 +0200

    Entidad lista

--- PrimerParcialDanny/Entidades/Articulos.cs
+++ PrimerParcialDanny/Entidades/Articulos.cs
@@ -8,11 +8,19 @@ namespace PrimerParcialDanny.Entidades
 {
     class Articulos
     {
-    
+        public int IDVendedor { get; set; }
+        public string Nombre { get; set; }
+        public  float Sueldo { get; set; }
+        public float Retencion { get; set; }
+        
 
         public Articulos()
         {
-            
+
+            IDVendedor = 0;
+            Nombre = string.Empty;
+            Sueldo = 0;
+            Retencion = 0;
 
         }
     }

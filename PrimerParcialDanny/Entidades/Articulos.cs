11ddbe3d376610d8f28630b33bf3f25bf9479f87
DANNY BERNARD <DANNY.BERNARD@hotmail.com>
2018-09-18 00:15:15 +0200

    Empezamos

--- PrimerParcialDanny/Entidades/Articulos.cs
+++ PrimerParcialDanny/Entidades/Articulos.cs
@@ -8,19 +8,11 @@ namespace PrimerParcialDanny.Entidades
 {
     class Articulos
     {
-        public int ID { get; set; }
-        public string Descipcion { get; set; }
-        public decimal Precio { get; set; }
-        public int Cantidad { get; set; }
-        public DateTime FechaDeVencimiento { get; set; }
+    
 
         public Articulos()
         {
-            ID = 0;
-            Descipcion = string.Empty;
-            Precio = 0;
-            Cantidad = 0;
-            FechaDeVencimiento = DateTime.Now;
+            
 
         }
     }
